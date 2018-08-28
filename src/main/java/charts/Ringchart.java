81cb0e2cd6de337c5bf017e1e5a1ab1b542f0b51
mare <maxi.rehberger@gmail.com>
2018-08-28 17:17:29 +0200

    Commit 3

--- src/main/java/charts/Ringchart.java
+++ src/main/java/charts/Ringchart.java
@@ -5,6 +5,14 @@ public class Ringchart {
 
     public static void main(String[] args) {
 
+        /** TODO
+        // CSV einlesen
+        // Datenbank schreiben
+        // Datenbank lesen
+        // Als Chart exportieren -> PDF
+        */
+
+        
     }
 
 }
