b92d36bcdf163c22aa3ed3477bd7b7432e5ebc0c
mare <maxi.rehberger@gmail.com>
2018-08-29 09:38:14 +0200

    Commit 5

--- src/main/java/charts/Piechart.java
+++ src/main/java/charts/Piechart.java
@@ -9,10 +9,18 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Piechart {
+
+	//Variablen
+
+	//Datenbankanbindung
+	private static final String url = "jdbc:postgresql://localhost/exercise";
+	private static final String user = "mare";
+	private static final String password = "toor";
 
 	//Pfad zur xls Datei
 	public static String EXAMPLE_XLS_FILE = "piechart-data.xls";
@@ -30,24 +38,31 @@ public class Piechart {
 		HSSFCell cell;
 
 		Iterator rows = sheet.rowIterator();
+		int j = 0;
 
 		while (rows.hasNext()) {
 			row = (HSSFRow) rows.next();
 			Iterator cells = row.cellIterator();
 
+			// Header überspringen (erste Zeile exel)
+			j++;
+			if(j == 1) continue;
+
 			//Country Objekt erstellen
 			Country c = new Country();
+			boolean isNull = false;
 
 			while (cells.hasNext()) {
 				cell = (HSSFCell) cells.next();
 
 				if (cell.getCellType() == HSSFCell.CELL_TYPE_STRING) {
-					System.out.print(cell.getStringCellValue() + " ");
 
 					//funktioniert nur in diesem Fall
-					c.setName(cell.getStringCellValue());
+					String stringcell = "";
+					stringcell += cell.getStringCellValue();
+					c.setName(stringcell);
+
 				} else if (cell.getCellType() == HSSFCell.CELL_TYPE_NUMERIC) {
-					System.out.print(cell.getNumericCellValue() + " ");
 
 					//funktioniert nur in diesem Fall
 					c.setWeight(cell.getNumericCellValue());
@@ -55,14 +70,54 @@ public class Piechart {
 				} else {
 					//U Can Handel Boolean, Formula, Errors
 				}
+
 			}
 
-			countries.add(c);
+			if(!isNull) {
+				//letzte Zeile (null-Zeile überspringen)
+				if(!c.nameIsNull()) {
+					countries.add(c);
+				}
+			}
 
-			System.out.println();
 		}
 
 		return countries;
+
+	}
+
+	// Datenbankverbindung aufbauen JDBC
+	public static Connection connect() {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			System.out.println("Connected to the PostgreSQL server successfully.");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+		return conn;
+	}
+
+	// Hilfsmethode für JDBC
+
+	// Methode um Liste an Countries in Datenbank zu schreiben (JDBC)
+	public static void writeDataToDatabase(ArrayList<Country> cl){
+
+		// Tabelle erstellen falls nicht existiert
+		String SQL = "CREATE TABLE IF NOT EXISTS CountryTable (name varchar(40), weight float(53));";
+
+		try(
+			Connection testconn = connect();
+			Statement stmt = testconn.createStatement();
+			ResultSet rs = stmt.executeQuery(SQL)
+		){
+			// nothing to do
+		} catch (SQLException e) {
+			// nothing to do
+		}
+
+
 
 	}
 
@@ -75,11 +130,19 @@ public class Piechart {
 		 * Als Chart exportieren -> PDF
 		 */
 
+		// XLS einlesen -> Countries
 		ArrayList<Country> countries = readXLSFile();
 
 		for(Country c : countries){
 			System.out.println(c.getName() + " - " + c.getWeight());
 		}
+
+		System.out.println();
+
+		// Daten in die Datenbank schrieben
+		writeDataToDatabase(countries);
+
+
 
 		/** TODO
 		 * 1. und letzte Zeile fixen
